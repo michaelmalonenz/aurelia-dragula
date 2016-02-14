1c94070583aa4311ff02ecdc6150a4dbe86aa777
michaelmalonenz <michael.j.b.malone@gmail.com>
2016-02-14 16:47:54 +1300

    Added the missing css and the other half of the options that I missed before

--- dist/system/index.js
+++ dist/system/index.js
@@ -1,14 +1,26 @@
-System.register([], function (_export) {
+System.register(['./aurelia/options'], function (_export) {
   'use strict';
+
+  var Options, GLOBAL_OPTIONS;
 
   _export('configure', configure);
 
-  function configure(config) {
-    config.globalResources('./dragula.js');
+  function configure(config, callback) {
+    config.globalResources(['./dragula.js', './aurelia/options']);
+
+    var defaults = new Options();
+    config.container.registerInstance(GLOBAL_OPTIONS, defaults);
+
+    if (callback !== undefined && typeof callback === 'function') {
+      callback(defaults);
+    }
   }
 
   return {
-    setters: [],
+    setters: [function (_aureliaOptions) {
+      Options = _aureliaOptions.Options;
+      GLOBAL_OPTIONS = _aureliaOptions.GLOBAL_OPTIONS;
+    }],
     execute: function () {}
   };
 });
