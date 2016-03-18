44e8687fc0c77c17b64d22b520cd2dd07ae4394c
michaelmalonenz <michael.j.b.malone@gmail.com>
2016-03-19 12:45:52 +1300

    Should complete the dragula-and-drop custom element
    allowing Dragula to work within aurelia's binding system

--- src/dragula-and-drop.js
+++ src/dragula-and-drop.js
@@ -1,13 +1,12 @@
 import {customElement, bindable, noView} from 'aurelia-templating';
 import {bindingMode} from 'aurelia-binding';
-import {inject} from 'aurelia-dependency-injection';
 
-import {Options, DIRECTION, GLOBAL_OPTIONS} from './options';
+import {Options} from './options';
 import {Dragula} from './dragula';
 
-@bindable({ name: 'moves', defaultBindingMode: bindingMode.oneTime })
-@bindable({ name: 'accepts', defaultBindingMode: bindingMode.oneTime })
-@bindable({ name: 'invalid', defaultBindingMode: bindingMode.oneTime })
+@bindable({ name: 'moves', defaultBindingMode: bindingMode.oneTime, defaultValue: Options.always })
+@bindable({ name: 'accepts', defaultBindingMode: bindingMode.oneTime, defaultValue: Options.always })
+@bindable({ name: 'invalid', defaultBindingMode: bindingMode.oneTime, defaultValue: Options.invalidTarget })
 @bindable({ name: 'containers', defaultBindingMode: bindingMode.oneTime })
 @bindable({ name: 'isContainer', attribute: 'is-container', defaultBindingMode: bindingMode.oneTime })
 @bindable({ name: 'copy', defaultBindingMode: bindingMode.oneTime })
@@ -17,23 +16,19 @@ import {Dragula} from './dragula';
 @bindable({ name: 'direction', defaultBindingMode: bindingMode.oneTime })
 @bindable({ name: 'ignoreInputTextSelection', attribute: 'ingore-input-text-selection', defaultBindingMode: bindingMode.oneTime })
 @bindable({ name: 'mirrorContainer', attribute: 'mirror-container', defaultBindingMode: bindingMode.oneTime })
+@bindable({ name: 'targetClass', attribute: 'target-class', defaultValue: 'drop-target', defaultBindingMode: bindingMode.oneTime })
+@bindable({ name: 'sourceClass', attribute: 'source-class', defaultValue: 'drag-source', defaultBindingMode: bindingMode.oneTime })
 @customElement('dragula-and-drop')
-@inject(GLOBAL_OPTIONS)
 @noView()
 export class DragulaAndDrop {
 
-  constructor(globalOptions) {
-    this.globalOptions = globalOptions;
+  constructor() {
     this.drake;
   }
 
   bind() {
     let boundOptions = {
-      moves: this.moves,
-      accepts: this.accepts,
-      invalid: this.invalid,
       containers: this.containers,
-      isContainer: this.isContainer,
       copy: this.copy,
       copySortSource: this.copySortSource,
       revertOnSpill: this.revertOnSpill,
@@ -43,11 +38,42 @@ export class DragulaAndDrop {
       mirrorContainer: this.mirrorContainer
     };
 
-    let options = Object.assign({}, this.globalOptions, boundOptions);
-    this.drake = new Dragula(options);
+    let aureliaOptions = {
+      isContainer: el => {
+        if (!el) {
+          return false;
+        }
+        if (typeof this.isContainer === 'function') {
+          return this.isContainer({ el: el });
+        }
+
+        if (this.dragula.dragging) {
+          return el.classList.contains(this.targetClass);
+        }
+        return el.classList.contains(this.sourceClass);
+      },
+      moves: (item, source, handle, sibling) => {
+        return this.moves({ item: item, source: source, handle: handle, sibling: sibling });
+      },
+      accepts: (item, target, source, currentSibling) => {
+        return this.accepts({ item: item, target: target, source: source, currentSibling });
+      },
+      invalid: (item, handle) => {
+        return this.invalid({ item: item, handle: handle });
+      }
+    };
+
+    let options = Object.assign(aureliaOptions, boundOptions);
+    this.dragula = new Dragula(options);
+
+    this.dragula.on('drop', (el, target, source, sibling) => {
+      this.dragula.cancel();
+      this.dropFn({ el: el, target: target, source: source, sibling: sibling });
+    });
   }
 
   unbind() {
-    this.drake.destroy();
+    this.dragula.destroy();
   }
+
 }
