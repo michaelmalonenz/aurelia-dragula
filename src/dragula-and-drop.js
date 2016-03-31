984441506f96dd5119c063464214494cb6116289
michaelmalonenz <michael.j.b.malone@gmail.com>
2016-03-31 22:26:28 +1300

    Paranoia to help solve the binding issue

--- src/dragula-and-drop.js
+++ src/dragula-and-drop.js
@@ -45,11 +45,7 @@ export class DragulaAndDrop {
     let options = Object.assign(aureliaOptions, boundOptions);
     this.dragula = new Dragula(options);
 
-    this.dragula.on('drop', (item, target, source, sibling) => {
-      this.dragula.cancel();
-      if (typeof this.dropFn === 'function')
-        this.dropFn({ item: item, target: target, source: source, sibling: sibling });
-    });
+    this.dragula.on('drop', this._dropFunction.bind(this));
 
     this.dragula.on('drag', (item, source) => {
       if (typeof this.dragFn === 'function')
@@ -64,6 +60,12 @@ export class DragulaAndDrop {
 
   unbind() {
     this.dragula.destroy();
+  }
+
+  _dropFunction(item, target, source, sibling) {
+    this.dragula.cancel();
+    if (typeof this.dropFn === 'function')
+      this.dropFn({ item: item, target: target, source: source, sibling: sibling });
   }
 
   _isContainer(el) {
