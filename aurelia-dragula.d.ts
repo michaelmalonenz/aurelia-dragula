05ecd755677001b95e5727ed9b7dac3023fed083
Michael Malone <michael.j.b.malone@gmail.com>
2016-09-10 10:32:52 +1200

    update the typings, hopefully getting them right...

--- aurelia-dragula.d.ts
+++ aurelia-dragula.d.ts
@@ -1,46 +1,46 @@
 declare namespace dragula {
-    
-    export interface DragulaOptions {
-        isContainer?: (el?: Element) => boolean;       
-        moves?: (el?: Element, source?: Element, handle?: Element, sibling?: Element) => boolean;
-        accepts?: (el?: Element, target?: Element, source?: Element, sibling?: Element) => boolean;
-        invalid?: (el?: Element, handle?: Element) => boolean;
 
-        direction?: string;
-        copy?: boolean;
-        copySortSource?: boolean;
-        revertOnSpill?: boolean;
-        removeOnSpill?: boolean;
-        mirrorContainer?: Element;
-        ignoreInputTextSelection?: boolean;
+  export interface Options {
+    isContainer?: (el?: Element) => boolean;
+    moves?: (el?: Element, source?: Element, handle?: Element, sibling?: Element) => boolean;
+    accepts?: (el?: Element, target?: Element, source?: Element, sibling?: Element) => boolean;
+    invalid?: (el?: Element, handle?: Element) => boolean;
 
-        // Setting this option is effectively the same as passing the containers in the first argument to dragula(containers, options).
-        containers?: Array<Element>;
-    }
+    direction?: string;
+    copy?: boolean;
+    copySortSource?: boolean;
+    revertOnSpill?: boolean;
+    removeOnSpill?: boolean;
+    mirrorContainer?: Element;
+    ignoreInputTextSelection?: boolean;
 
-    export interface Drake {
-        containers: Array<Element>;
-        dragging: boolean;
-        
-        start(item: Element): void;
-        end(): void;
-        cancel(revert?: boolean): void;
-        remove(): void;
-        on(events: string, callback: Function): void;
-        off(events: string, callback: Function): void;
-        once(events: string, callback: Function): void;
-        canMove(item: Element): boolean;
-        destroy(): void;
-    }
+    // Setting this option is effectively the same as passing the containers in the first argument to dragula(containers, options).
+    containers?: Array<Element>;
+  }
 
-    export interface Dragula {
-        new (containers: Element|Array<Element>, options?: DragulaOptions): Drake;
-        new (options?: DragulaOptions): Drake;
-        new (): Drake;
-    }
+  export interface Dragula {
+    new (options?: Options): Dragula;
+    new (): Dragula;
+
+    containers: Array<Element>;
+    dragging: boolean;
+    options: Options;
+
+    start(item: Element): void;
+    end(): void;
+    cancel(revert?: boolean): void;
+    remove(): void;
+    on(events: string, callback: Function): void;
+    off(events: string, callback: Function): void;
+    once(events: string, callback: Function): void;
+    canMove(item: Element): boolean;
+    destroy(): void;
+    isContainer(item: Element);
+    drop(item: Element, target: Element);
+  }
+
+  function moveBefore(array: Array<any>, itemMatcherFn: (item: Element) => boolean, siblingMatcherFn: (item: Element) => boolean)
 }
-
-declare let Dragula: dragula.Dragula;
 
 declare module "aurelia-dragula" {
     export default Dragula;
