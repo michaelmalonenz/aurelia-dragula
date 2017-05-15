fca8b400ba296bce7fd3504fb8145162c078f189
Michael Malone <michael.j.b.malone@gmail.com>
2017-05-15 21:30:36 +1200

    v1.4.3

--- dist/aurelia-dragula.js
+++ dist/aurelia-dragula.js
@@ -61,8 +61,8 @@ export function rm(el, className) {
 @bindable({ name: 'overFn', attribute: 'over-fn', defaultBindingMode: bindingMode.oneTime })
 @bindable({ name: 'outFn', attribute: 'out-fn', defaultBindingMode: bindingMode.oneTime })
 @bindable({ name: 'shadowFn', attribute: 'shadow-fn', defaultBindingMode: bindingMode.oneTime })
-@useView(PLATFORM.moduleName('./dragula-and-drop'))
 @customElement('dragula-and-drop')
+@useView('./dragula-and-drop.html')
 @inject(GLOBAL_OPTIONS)
 export class DragulaAndDrop {
 
