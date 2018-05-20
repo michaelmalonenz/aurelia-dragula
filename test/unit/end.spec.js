d32c5775bf0fee3546d255a62c2eafed812e89e8
Michael Malone <michael.malone@aranzgeo.com>
2018-05-20 19:01:24 +1200

    Conform to Standard

--- test/unit/end.spec.js
+++ test/unit/end.spec.js
@@ -1,87 +1,86 @@
-import {createDragula} from './lib/create-dragula';
+/* global describe, beforeEach, afterEach, it, expect */
+import {createDragula} from './lib/create-dragula'
 
-describe('end', function() {
+describe('end', function () {
+  describe('does not throw when not dragging', function () {
+    beforeEach(function () {
+      this.drake = createDragula()
+    })
 
-  describe('does not throw when not dragging', function() {
+    afterEach(function () {
+      this.drake.destroy()
+    })
 
-    beforeEach(function() {
-      this.drake = createDragula();
-    });
-
-    afterEach(function() {
-      this.drake.destroy();
-    });
-
-    it('a single time', function() {
+    it('a single time', function () {
       let test = () => this.drake.end()
 
-      expect(test).not.toThrow();
-    });
+      expect(test).not.toThrow()
+    })
 
-    it('multiple times', function() {
+    it('multiple times', function () {
       let test = () => {
-        this.drake.end();
-        this.drake.end();
-        this.drake.end();
-        this.drake.end();
-      };
+        this.drake.end()
+        this.drake.end()
+        this.drake.end()
+        this.drake.end()
+      }
 
-      expect(test).not.toThrow();
-    });
-  });
+      expect(test).not.toThrow()
+    })
+  })
 
-  beforeEach(function() {
-    this.div = document.createElement('div');
-    this.item1 = document.createElement('div');
-    this.item2 = document.createElement('div');
+  beforeEach(function () {
+    this.div = document.createElement('div')
+    this.item1 = document.createElement('div')
+    this.item2 = document.createElement('div')
 
-    this.div.appendChild(this.item1);
-    this.div.appendChild(this.item2);
-    document.body.appendChild(this.div);
-  });
+    this.div.appendChild(this.item1)
+    this.div.appendChild(this.item2)
+    document.body.appendChild(this.div)
+  })
 
-  it('when already dragging, .end() ends (cancels) previous drag', function() {
-    var drake = createDragula([this.div]);
+  it('when already dragging, .end() ends (cancels) previous drag', function () {
+    var drake = createDragula([this.div])
 
-    drake.manualStart(this.item1);
+    drake.manualStart(this.item1)
 
     drake.on('dragend', (item) => {
-      expect(item).toBe(this.item1, 'dragend invoked with correct item');
-    });
+      expect(item).toBe(this.item1, 'dragend invoked with correct item')
+    })
     drake.on('cancel', (item, source) => {
-      expect(item).toBe(this.item1, 'cancel invoked with correct item');
-      expect(source).toBe(this.div, 'cancel invoked with correct source');
-    });
+      expect(item).toBe(this.item1, 'cancel invoked with correct item')
+      expect(source).toBe(this.div, 'cancel invoked with correct source')
+    })
 
-    drake.end();
+    drake.end()
 
-    expect(drake.dragging).toBeFalsy('final state is: drake is not dragging');
+    expect(drake.dragging).toBeFalsy('final state is: drake is not dragging')
 
-    drake.destroy();
-  });
+    drake.destroy()
+  })
 
-  it('when already dragged, ends (drops) previous drag', function() {
-    var div2 = document.createElement('div');
-    var drake = createDragula([this.div, this.div2]);
-    document.body.appendChild(div2);
+  it('when already dragged, ends (drops) previous drag', function () {
+    var div2 = document.createElement('div')
+    var drake = createDragula([this.div, this.div2])
+    document.body.appendChild(div2)
 
-    drake.manualStart(this.item1);
+    drake.manualStart(this.item1)
 
-    div2.appendChild(this.item1);
+    div2.appendChild(this.item1)
 
     drake.on('dragend', (item) => {
-      expect(item).toBe(this.item1, 'dragend invoked with correct item');
-    });
+      expect(item).toBe(this.item1, 'dragend invoked with correct item')
+    })
     drake.on('drop', (item, target, source) => {
-      expect(item).toBe(this.item1, 'drop invoked with correct item');
-      expect(source).toBe(this.div, 'drop invoked with correct source');
-      expect(target).toBe(div2, 'drop invoked with correct target');
-    });
+      expect(item).toBe(this.item1, 'drop invoked with correct item')
+      expect(source).toBe(this.div, 'drop invoked with correct source')
+      expect(target).toBe(div2, 'drop invoked with correct target')
+    })
 
-    drake.end();
+    drake.end()
 
-    expect(drake.dragging).toBeFalsy('final state is: drake is not dragging');
+    expect(drake.dragging).toBeFalsy('final state is: drake is not dragging')
 
-    drake.destroy();
-  });
-});
+    drake.destroy()
+  })
+})
--- test/unit/lib/create-dragula.js
+++ test/unit/lib/create-dragula.js
@@ -1,8 +1,8 @@
-import {Dragula} from '../../../src/dragula';
-import {Options} from '../../../src/options';
+import {Dragula} from '../../../src/dragula'
+import {Options} from '../../../src/options'
 
-export function createDragula(initialContainers, options = {}) {
-  let opts = Object.assign(new Options(), options);
-  opts.containers = initialContainers || [];
-  return new Dragula(opts);
+export function createDragula (initialContainers, options = {}) {
+  let opts = Object.assign(new Options(), options)
+  opts.containers = initialContainers || []
+  return new Dragula(opts)
 }
