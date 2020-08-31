1233c3e2438da392c911687cff69040cad33148c
Michael Malone <michael.j.b.malone@gmail.com>
2020-08-31 20:00:19 +1200

    blanket package update
    No idea if this is even a smart idea

--- config.js
+++ config.js
@@ -7,54 +7,48 @@ System.config({
   },
 
   map: {
-    "aurelia-binding": "npm:aurelia-binding@1.7.3",
-    "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.4.1",
-    "aurelia-logging": "npm:aurelia-logging@1.5.0",
-    "aurelia-pal": "npm:aurelia-pal@1.8.0",
-    "aurelia-pal-browser": "npm:aurelia-pal-browser@1.8.0",
-    "aurelia-polyfills": "npm:aurelia-polyfills@1.3.0",
-    "aurelia-templating": "npm:aurelia-templating@1.10.0",
-    "npm:aurelia-binding@1.7.3": {
-      "aurelia-logging": "npm:aurelia-logging@1.5.0",
-      "aurelia-metadata": "npm:aurelia-metadata@1.0.4",
-      "aurelia-pal": "npm:aurelia-pal@1.8.0",
-      "aurelia-task-queue": "npm:aurelia-task-queue@1.3.1"
+    "aurelia-binding": "npm:aurelia-binding@2.5.4",
+    "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.5.2",
+    "aurelia-logging": "npm:aurelia-logging@1.5.2",
+    "aurelia-pal": "npm:aurelia-pal@1.8.2",
+    "aurelia-pal-browser": "npm:aurelia-pal-browser@1.8.1",
+    "aurelia-polyfills": "npm:aurelia-polyfills@1.3.4",
+    "aurelia-templating": "npm:aurelia-templating@1.10.4",
+    "npm:aurelia-binding@2.5.4": {
+      "aurelia-logging": "npm:aurelia-logging@1.5.2",
+      "aurelia-metadata": "npm:aurelia-metadata@1.0.6",
+      "aurelia-pal": "npm:aurelia-pal@1.8.2",
+      "aurelia-task-queue": "npm:aurelia-task-queue@1.3.3"
     },
-    "npm:aurelia-binding@2.1.5": {
-      "aurelia-logging": "npm:aurelia-logging@1.5.0",
-      "aurelia-metadata": "npm:aurelia-metadata@1.0.4",
-      "aurelia-pal": "npm:aurelia-pal@1.8.0",
-      "aurelia-task-queue": "npm:aurelia-task-queue@1.3.1"
+    "npm:aurelia-dependency-injection@1.5.2": {
+      "aurelia-metadata": "npm:aurelia-metadata@1.0.6",
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
     },
-    "npm:aurelia-dependency-injection@1.4.1": {
-      "aurelia-metadata": "npm:aurelia-metadata@1.0.4",
-      "aurelia-pal": "npm:aurelia-pal@1.8.0"
+    "npm:aurelia-loader@1.0.2": {
+      "aurelia-metadata": "npm:aurelia-metadata@1.0.6",
+      "aurelia-path": "npm:aurelia-path@1.1.5"
     },
-    "npm:aurelia-loader@1.0.0": {
-      "aurelia-metadata": "npm:aurelia-metadata@1.0.4",
-      "aurelia-path": "npm:aurelia-path@1.1.1"
+    "npm:aurelia-metadata@1.0.6": {
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
     },
-    "npm:aurelia-metadata@1.0.4": {
-      "aurelia-pal": "npm:aurelia-pal@1.8.0"
+    "npm:aurelia-pal-browser@1.8.1": {
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
     },
-    "npm:aurelia-pal-browser@1.8.0": {
-      "aurelia-pal": "npm:aurelia-pal@1.8.0"
+    "npm:aurelia-polyfills@1.3.4": {
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
     },
-    "npm:aurelia-polyfills@1.3.0": {
-      "aurelia-pal": "npm:aurelia-pal@1.8.0"
+    "npm:aurelia-task-queue@1.3.3": {
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
     },
-    "npm:aurelia-task-queue@1.3.1": {
-      "aurelia-pal": "npm:aurelia-pal@1.8.0"
-    },
-    "npm:aurelia-templating@1.10.0": {
-      "aurelia-binding": "npm:aurelia-binding@2.1.5",
-      "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.4.1",
-      "aurelia-loader": "npm:aurelia-loader@1.0.0",
-      "aurelia-logging": "npm:aurelia-logging@1.5.0",
-      "aurelia-metadata": "npm:aurelia-metadata@1.0.4",
-      "aurelia-pal": "npm:aurelia-pal@1.8.0",
-      "aurelia-path": "npm:aurelia-path@1.1.1",
-      "aurelia-task-queue": "npm:aurelia-task-queue@1.3.1"
+    "npm:aurelia-templating@1.10.4": {
+      "aurelia-binding": "npm:aurelia-binding@2.5.4",
+      "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.5.2",
+      "aurelia-loader": "npm:aurelia-loader@1.0.2",
+      "aurelia-logging": "npm:aurelia-logging@1.5.2",
+      "aurelia-metadata": "npm:aurelia-metadata@1.0.6",
+      "aurelia-pal": "npm:aurelia-pal@1.8.2",
+      "aurelia-path": "npm:aurelia-path@1.1.5",
+      "aurelia-task-queue": "npm:aurelia-task-queue@1.3.3"
     }
   }
 });
